2aad7196833de349e44195314bd2f64384f7d808
Dmytro Prontenko <136347904+dmytro-prontenko@users.noreply.github.com>
2025-01-16 16:49:40 +0100

    feat: header contacts hovering behaviour added

--- js/index.js
+++ js/index.js
@@ -3,3 +3,16 @@ import mobileMenu from "./mobile-menu.js";
 document.addEventListener("DOMContentLoaded", () => {
   mobileMenu();
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const callBtn = document.querySelector(".header__call-btn");
+  const contactsSection = document.querySelector("#contacts");
+
+  callBtn.addEventListener("click", function () {
+    // Перевіряємо ширину екрану
+    if (window.innerWidth < 1024) {
+      // Плавний скрол до секції контактів
+      contactsSection.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
